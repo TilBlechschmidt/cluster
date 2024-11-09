ff7c7e3f6b2d181b547f2adccdd71400442994d0
Til Blechschmidt <me@tibl.dev>
2024-11-09 19:05:16 +0100

    Replace pbkdf2 with argon2 for OIDC client secrets

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,9 +1,12 @@
 import { EnvValue, Volume } from 'cdk8s-plus-26';
-import { execSync } from 'child_process';
 import { Construct } from 'constructs';
 import { createHash } from 'crypto';
 import { pbkdf2Sync } from 'pbkdf2';
+import { hash, argon2id } from 'argon2';
+import sp from 'synchronized-promise'
 import secrets from '../secrets.json';
+
+const syncHash = sp(hash);
 
 const SALT = 'uznkf5beRZNGe+BafDxU1MvUYgCrj3M/BxrjPqaZGzhRFwq1/FCxdcemZ8Oo5vN9Kn8LlpBkNqfAs0eS4XM1ew==';
 const HOST_PATH = '/var/lib/volumes/';
@@ -20,20 +23,9 @@ export function generateURLSafeSecret(id: string, length: number): string {
 }
 
 export function generateAutheliaDigest(password: string): string {
-    // TODO Figure out a way to do base64 w/ custom alphabet so we don't have to call into authelia/Docker
-    //      https://sourceware.org/git/?p=glibc.git;a=blob;f=crypt/crypt_util.c;h=c9cf9ba59e457656f2eace768c0083c490b44805;hb=refs/heads/master#l250
-    const command = `authelia crypto hash generate pbkdf2 --no-confirm --password '${password}'`;
-    let output: string;
-
-    try {
-        output = execSync(command).toString();
-    } catch {
-        console.warn('Failed to call authelia, attempting to run it in docker ...');
-        output = execSync(`docker run --rm ghcr.io/authelia/authelia:4.37.5 ${command}`).toString();
-    }
-
-    if (output.indexOf('Digest: ') != 0) throw 'Authelia returned unexpected digest string: ' + output;
-    return output.substring('Digest: '.length).trim();
+    return syncHash(password, {
+        type: argon2id,
+    });
 }
 
 export function obj2env(env: { [name: string]: string }): { [name: string]: EnvValue } {
